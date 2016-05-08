aaa23d3ffeb60701081eab819d694a5f5d6f6cf3
Colin McDonald <mickydwow@gmail.com>
2016-05-07 17:01:55 -0700

    Added server to basic one

---
+++ basic/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var httpProxy = require('http-proxy');
+var publicPath = path.resolve(__dirname, 'public');
+
+var port = 3000;
+
+var app = express();
+
+//serving our index.html
+app.use(express.static(publicPath));
+
+app.listen(port, function () {
+  console.log('Server running on port ' + port)
+});
