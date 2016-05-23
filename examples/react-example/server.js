0ea2aef1e0f8629cc85dd08ea426d12f321c47cd
Colin McDonald <mickydwow@gmail.com>
2016-05-23 10:44:35 -0700

    Added comments to server.js

--- examples/react-example/server.js
+++ examples/react-example/server.js
@@ -16,13 +16,13 @@ var app = express();
 //serving our index.html
 app.use(express.static(publicPath));
 
-//server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves
-//the compiler adds some console logs for some extra sugar
-//notice that you will not see a physical bundle.js because webpack-dev-server runs it from memory
+//server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves.
+//The compiler adds some console logs for some extra sugar.
+//Notice that you will not see a physical bundle.js because webpack-dev-server runs it from memory.
 var bundle = require('./server/compiler.js')
 bundle()
 
-//express now processes all requests to localhost:8080
+//express now processes all requests to localhost:8080/build/*
 //app.all is a special routing method used for loading middleware functions
 app.all('/build/*', function (req, res) {
   proxy.web(req, res, {
